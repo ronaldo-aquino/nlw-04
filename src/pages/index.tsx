1ff21bb9d853e65446f64ead8615298ef495cf4b
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-23 15:33:42 -0300

    Aplicação com next

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import ExperienceBar from "../components/ExperienceBar";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+};
+
+export default Home;
