c347ea0b3d1ba4a69ad4f3de62c189007e480248
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-23 19:44:25 -0300

    Componente Countdown finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,23 @@ import styles from "../styles/pages/Home.module.css";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
+
+import Head from "next/head"
 
 const Home = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
